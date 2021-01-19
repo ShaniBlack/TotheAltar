3dc98784807444c563ddd7d7ce101d8b27e5b209
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-19 08:17:06 -0600

    created vendor card items

---
+++ client/src/components/VendorCard/VendorCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+function VendorCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Occupation:</strong> {props.occupation}
+          </li>
+          <li>
+            <strong>Location:</strong> {props.location}
+          </li>
+        </ul>
+      </div>
+      <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+      </span>
+    </div>
+  );
+}
+
+export default VendorCard;
