8a60ecb9a29fcadbae6ed4763a96ed25b3525ebf
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-21 00:40:15 -0600

    added file to route from other file that routes from server.js

---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const vendorRoutes = require("./vendor-routes");
+const eventRoutes = require("./event-routes");
+const usersRoutes = require("./users-routes");
+
+// routes
+router.use("/vendors", vendorRoutes);
+router.use("/events", eventRoutes);
+router.use("/users", usersRoutes);
+
+module.exports = router;
