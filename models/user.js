5559b461f42a192e4b5c1e8755d310cca3f998b4
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-21 10:28:19 -0600

    created login and associated it to users, added hasOne to user

--- models/user.js
+++ models/user.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Event)
+      User.hasOne(models.Login)
     }
   };
   User.init({
