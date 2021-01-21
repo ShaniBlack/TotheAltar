fcba81a5f61b2327b1eb1502d392da592f10c5cf
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-21 09:35:45 -0600

    create migration for models

---
+++ migrations/20210121142937-create-test.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('tests', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      a: {
+        type: Sequelize.STRING
+      },
+      b: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('tests');
+  }
+};
