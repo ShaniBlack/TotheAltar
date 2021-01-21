70b71430150f997f028b3fcfc62e33b2408ea6eb
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-21 10:08:02 -0600

    added association between user and event

--- models/event.js
+++ models/event.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      // Event.belongsTo(models.users)
+      Event.belongsTo(models.User)
     }
   };
   Event.init({
---
+++ models/user.js
@@ -0,0 +1,39 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Event)
+    }
+  };
+  User.init({
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+  },
+  last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+  },
+  email: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isEmail: true,
+        len: [1, 50],
+      },
+  },
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
