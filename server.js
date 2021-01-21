49005d880d07d7d0651024f55cc29e5c2f417f52
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-20 21:24:50 -0600

    activated routes, disabled DB Sync

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const express = require('express');
 // const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
 
 // Requiring our models for syncing
-var db = require("./models");
+//var db = require("./models");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -16,14 +17,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 // Add routes, both API and view
-// app.use(routes);
+app.use(routes);
 
-
-// All you have to do is make sure that you have an .env file with input values for the atlas db connection and plug that into heroku upon deployment as well
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
