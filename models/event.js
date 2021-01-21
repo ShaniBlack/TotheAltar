e29d7693c702eae89bfee3fd012d9317ac344e69
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-21 10:23:12 -0600

    added belongsToMany for event and vendor, and creates new VendorEvent table

--- models/event.js
+++ models/event.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Event.belongsTo(models.User)
+      Event.belongsToMany(models.Vendor, { through: 'VendorEvent' });
     }
   };
   Event.init({
--- models/vendor.js
+++ models/vendor.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Vendor.belongsTo(models.Event)
+     Vendor.belongsToMany(models.Event, { through: 'VendorEvent' });
     }
   };
   Vendor.init({
