6baae64d311b30e9e30a398d467807d693af5f5c
Ryan-Henson <72321799+Ryan-Henson@users.noreply.github.com>
2021-01-21 19:37:27 -0600

    changed loggin to auth0 instead of housing loggin info in server info.

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import './index.css';
-import App from './App';
-import "react-bulma-components/dist/react-bulma-components.min.css"
+import App from "./App";
+import "react-bulma-components/dist/react-bulma-components.min.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-n3kobf7i.us.auth0.com"
+      clientId="4BROrd2JbYA8HoJL9UxwHqa9Twm7tX2k"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Events() {
-    return (
-        <div className="bg-img">
-            <figure className="image">
-                <img src="https://images.unsplash.com/photo-1513725673171-537abba17912?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjR8fHdlZGRpbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">
-                    </img>
-            </figure>
-            
-        </div>
-    )
+  return (
+    <div className="bg-img">
+      <figure className="image">
+        <img src="https://images.unsplash.com/photo-1513725673171-537abba17912?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjR8fHdlZGRpbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"></img>
+      </figure>
+    </div>
+  );
 }
