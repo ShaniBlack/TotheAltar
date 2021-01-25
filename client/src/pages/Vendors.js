6c416ea7f0f9adbff1b06147772fdf95d9ca1006
Shani Black <shaniblack2004@gmail.com>
2021-01-25 14:31:59 -0600

    added code to fix menu on vendors and wrapped vendor cards in container

--- client/src/pages/Vendors.js
+++ client/src/pages/Vendors.js
@@ -3,97 +3,100 @@ import VendorCard from "../components/VendorCard/VendorCard";
 import API from "../utils/API";
 import "react-bulma-components/dist/react-bulma-components.min.css";
 import "./Vendors.css";
-import Collapsible from "react-collapsible";
+// import Collapsible from "react-collapsible";
 
-export default function Vendors() {
+export default function Vendors(props) {
   const [vendors, setVendors] = useState([]);
-
+  const [filteredVendors, setFilteredVendors] = useState([]);
+  const [category, setCategory] = useState("");
+  const categories = [
+    "Bakery",
+    "Catering",
+    "Florist",
+    "Jeweler",
+    "Music",
+    "Officiator",
+    "Rentals",
+    "Venue",
+    "Wedding Attire",
+    "Miscellaneous",
+  ];
   useEffect(() => {
     loadVendors();
   }, []);
 
+  useEffect(() => {
+    setFilteredVendors(
+      vendors.filter((vendor) => vendor.category === category)
+    );
+  }, [category, vendors]);
+
   function loadVendors() {
     API.getVendors()
-      .then((res) => setVendors(res.data))
+      .then((res) => {
+        setVendors(res.data);
+      })
       .catch((err) => console.log(err));
   }
 
-  function deleteCard() {
-    API.deleteVendor()
+  function deleteCard(id) {
+    API.deleteVendor(id)
       .then((res) => loadVendors())
       .catch((err) => console.log(err));
   }
 
   return (
     <>
-      <section className="hero">
-        <div className="hero-body has-bg-image is-small">
-          <Collapsible trigger="Bakery">
-            <section className="level">
-                <div className="columns">
-                  <h1 className="title"></h1>
-                  {vendors.map((vendor) => (
-                    <div className="column is-one-quarter">
-                      <VendorCard
-                        deleteCard={deleteCard}
-                        id={vendor.id}
-                        key={vendor.id}
-                        vendor={vendor.vendor_name}
-                        contact={vendor.contact_name}
-                        phone={vendor.phone_number}
-                        projected_cost={vendor.projected_cost}
-                        actual_cost={vendor.actual_cost}
-                        notes={vendor.notes}
-                      />
-                    </div>
-                  ))}
-                </div>
-            </section>
-          </Collapsible>
+      <div className="columns is-multiline is-fullheight">
+        <div className="column-gap is-1 position">
+          <aside className="menu">
+            <ul class="menu-list">
+              {categories.map((category) => (
+                <a onClick={() => setCategory(category)}>{category}</a>
+              ))}
+            </ul>
+          </aside>
         </div>
-      </section>
 
-      <section className="hero">
-        <div className="hero-body has-bg-image is-small">
-          <Collapsible trigger="Catering">
-            <section className="level">
-                <div className="columns">
-                  <h1 className="title"></h1>
-                  {vendors.map((vendor) => (
-                    <div className="column is-one-quarter">
-                      <VendorCard />
-                    </div>
-                  ))}
-                </div>
-            </section>
-          </Collapsible>
+        <div className="container">
+          <div className="columns is-multiline">
+            {filteredVendors.map((vendor) => (
+              <div className="column is-5">
+                <VendorCard
+                  className="box"
+                  deleteCard={deleteCard}
+                  id={vendor.id}
+                  key={vendor.id}
+                  vendor={vendor.vendor_name}
+                  contact={vendor.contact_name}
+                  phone={vendor.phone_number}
+                  projected_cost={vendor.projected_cost}
+                  actual_cost={vendor.actual_cost}
+                  notes={vendor.notes}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      </section>
+      </div>
 
-      <section className="hero">
+      {/* <section className="hero">
+        <div className="hero-body has-bg-image is-small"> */}
+      {/* <Collapsible trigger="Bakery" onOpening = {() => setCategory("Bakery")}> */}
+      {/* <a onClick={() => setCategory("Bakery")}>Bakery</a> */}
+      {/* <section className="level"> */}
+      {/* </section> */}
+      {/* </Collapsible> */}
+      {/* </div>
+      </section> */}
+
+      {/* <section className="hero">
         <div className="hero-body has-bg-image is-small">
-          <Collapsible trigger="Florist">
+          <Collapsible trigger="Florist" onOpen = {() => setCategory("Florist")}>
             <section className="level">
                 <div className="columns">
                   <h1 className="title"></h1>
-                  {vendors.map((vendor) => (
-                    <div className="column is-one-quarter">
-                      <VendorCard />
-                    </div>
-                  ))}
-                </div>
-            </section>
-          </Collapsible>
-        </div>
-      </section>
-
-      <section className="hero">
-        <div className="hero-body has-bg-image is-small">
-          <Collapsible trigger="Gown/Tux Shop 1">
-            <section className="level">
-                <div className="columns">
-                  <h1 className="title"></h1>
-                  {vendors.map((vendor) => (
+                  {filteredVendors.map((vendor) => (
                     <div className="column is-one-quarter">
                       <VendorCard />
                     </div>
@@ -106,11 +109,11 @@ export default function Vendors() {
 
       <section className="hero">
         <div className="hero-body has-bg-image is-small">
-        <Collapsible trigger="Gown/Tux Shop 2">
+          <Collapsible trigger="Gown/Tux Shop 1" onOpen = {() => setCategory("Gown/Tux1")}>
             <section className="level">
                 <div className="columns">
                   <h1 className="title"></h1>
-                  {vendors.map((vendor) => (
+                  {filteredVendors.map((vendor) => (
                     <div className="column is-one-quarter">
                       <VendorCard />
                     </div>
@@ -122,12 +125,29 @@ export default function Vendors() {
       </section>
 
       <section className="hero">
-        <div className="hero-body has-bg-image is-small" id="jeweler">
+        <div className="hero-body has-bg-image is-small">
+        <Collapsible trigger="Gown/Tux Shop 2" onOpen = {() => setCategory("Gown/Tux2")}>
+            <section className="level">
+                <div className="columns">
+                  <h1 className="title"></h1>
+                  {filteredVendors.map((vendor) => (
+                    <div className="column is-one-quarter">
+                      <VendorCard />
+                    </div>
+                  ))}
+                </div>
+            </section>
+          </Collapsible>
+        </div>
+      </section>
+
+      <section className="hero">
+        <div className="hero-body has-bg-image is-small" id="Jeweler">
           <Collapsible trigger="Jeweler">
           <section className="level">
                 <div className="columns">
                   <h1 className="title"></h1>
-                  {vendors.map((vendor) => (
+                  {filteredVendors.map((vendor) => (
                     <div className="column is-one-quarter">
                       <VendorCard />
                     </div>
@@ -140,11 +160,11 @@ export default function Vendors() {
 
       <section className="hero">
         <div className="hero-body has-bg-image is-small">
-          <Collapsible trigger="Music">
+          <Collapsible trigger="Music" onOpen = {() => setCategory("Music")}>
           <section className="level">
                 <div className="columns">
                   <h1 className="title"></h1>
-                  {vendors.map((vendor) => (
+                  {filteredVendors.map((vendor) => (
                     <div className="column is-one-quarter">
                       <VendorCard />
                     </div>
@@ -157,11 +177,11 @@ export default function Vendors() {
 
       <section className="hero">
         <div className="hero-body has-bg-image is-small">
-          <Collapsible trigger="Officiator">
+          <Collapsible trigger="Officiator" onOpen = {() => setCategory("Officiator")}>
           <section className="level">
                 <div className="columns">
                   <h1 className="title"></h1>
-                  {vendors.map((vendor) => (
+                  {filteredVendors.map((vendor) => (
                     <div className="column is-one-quarter">
                       <VendorCard />
                     </div>
@@ -174,11 +194,11 @@ export default function Vendors() {
 
       <section className="hero">
         <div className="hero-body has-bg-image is-small">
-          <Collapsible trigger="Rentals">
+          <Collapsible trigger="Rentals" onOpen = {() => setCategory("Rentals")}>
           <section className="level">
                 <div className="columns">
                   <h1 className="title"></h1>
-                  {vendors.map((vendor) => (
+                  {filteredVendors.map((vendor) => (
                     <div className="column is-one-quarter">
                       <VendorCard />
                     </div>
@@ -191,11 +211,11 @@ export default function Vendors() {
 
       <section className="hero">
         <div className="hero-body has-bg-image is-small">
-          <Collapsible trigger="Venue">
+          <Collapsible trigger="Venue" onOpen = {() => setCategory("Venue")}>
             <section className="level">
                 <div className="columns">
                   <h1 className="title"></h1>
-                  {vendors.map((vendor) => (
+                  {filteredVendors.map((vendor) => (
                     <div className="column is-one-quarter">
                       <VendorCard />
                     </div>
@@ -206,13 +226,13 @@ export default function Vendors() {
         </div>
       </section>
 
-      <section className="hero">
+      <section className="hero"> 
         <div className="hero-body has-bg-image is-small">
-          <Collapsible trigger="Miscellaneous">
+          <Collapsible trigger="Miscellaneous" onOpen = {() => setCategory("Miscellaneous")}>
           <section className="level">
                 <div className="columns">
                   <h1 className="title"></h1>
-                  {vendors.map((vendor) => (
+                  {filteredVendors.map((vendor) => (
                     <div className="column is-one-quarter">
                       <VendorCard />
                     </div>
@@ -221,9 +241,11 @@ export default function Vendors() {
             </section>
           </Collapsible>
         </div>
-      </section>
+      </section>*/}
     </>
   );
 }
 
-// <Collapsible trigger={vendors.map((category) => (category=category.category))}>
+{
+  /* // <Collapsible trigger={vendors.map((category) => (category=category.category))}> */
+}
