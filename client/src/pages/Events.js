5d1d466b4bc03a866da909c2a36bb5f63d051ee8
Shani Black <shaniblack2004@gmail.com>
2021-01-25 15:41:05 -0600

    added heroClick from Events page to Vendors page

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
 import "./Events.css";
+import { useHistory } from "react-router-dom";
 // import "./event-form"
 
 import { useAuth0 } from "@auth0/auth0-react";
@@ -8,6 +9,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 export default function Events() {
   const { user } = useAuth0();
   const [events, setEvents] = useState([]);
+  const history = useHistory();
+
 
   useEffect(() => {
     loadEvents();
@@ -15,9 +18,17 @@ export default function Events() {
 
   function loadEvents() {
     API.getEvents()
-    .then((res) => setEvents(res.data))
-    .catch((err) => console.log(err));
+      .then((res) => setEvents(res.data))
+      .catch((err) => console.log(err));
   }
+
+  const heroClick = (id) => {
+   history.push("/vendors")};
+    // API.getEvent(id)
+    // .then((res) => setEvents(res.data))
+    // .catch((err) => console.log(err));
+    
+
 
   return (
     <>
@@ -31,23 +42,29 @@ export default function Events() {
         </div>
       </section>
 
-      <section className="hero">
-        <div className="hero-body has-bg-image is-medium">
-          <div className="container has-text-centered is-3 is-fullhd is-4-desktop is-12-tablet is-12-mobile has-text-black">
-            <h1 className="title" id="hero-font"></h1>
-              {events.map((event) => (
-                <div className="wrapper">
-                <h1 className="columns is-centered has-text-weight-bold" id="event-font">
+      {events.map((event) => (
+        <section className="hero">
+          <div className="hero-body has-bg-image is-medium">
+            <div className="container has-text-centered is-3 is-fullhd is-4-desktop is-12-tablet is-12-mobile has-text-black">
+              <h1 className="title" id="hero-font"></h1>
+              <div className="wrapper" onClick={heroClick}>
+                <h1
+                  className="columns is-centered has-text-weight-bold"
+                  id="event-font"
+                >
                   {event.event_name}
                 </h1>
-                <h1 className="columns is-centered has-text-weight-bold" id="event-font">
+                <h1
+                  className="columns is-centered has-text-weight-bold"
+                  id="event-font"
+                >
                   {event.event_date}
-                </h1>  
-                </div>              
-                ))}
+                </h1>
+              </div>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      ))}
     </>
   );
 }
