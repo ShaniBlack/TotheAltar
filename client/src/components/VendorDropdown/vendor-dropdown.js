7d7625219ec35525c4d768d1be2d7af03e2a0a05
Shani Black <shaniblack2004@gmail.com>
2021-01-25 02:27:00 -0600

    added to Events event name and date from db

---
+++ client/src/components/VendorDropdown/vendor-dropdown.js
@@ -0,0 +1,22 @@
+// to add to Vendor Form
+
+export default function VendorDropdown() {
+  <div className="control">
+    <div className="select" id="category-menu">
+      <select>
+        <option>Select Category</option>
+        <option>Bakery</option>
+        <option>Catering</option>
+        <option>Florist</option>
+        <option>Gown/Tux Shop 1</option>
+        <option>Gown/Tux Shop 2</option>
+        <option>Jeweler</option>
+        <option>Music</option>
+        <option>Officiator</option>
+        <option>Rentals</option>
+        <option>Venue</option>
+        <option>Miscellaneous</option>
+      </select>
+    </div>
+  </div>;
+}
--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
 import "./Events.css";
 // import "./event-form"
 
@@ -6,6 +7,18 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Events() {
   const { user } = useAuth0();
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    loadEvents();
+  }, []);
+
+  function loadEvents() {
+    API.getEvents()
+    .then((res) => setEvents(res.data))
+    .catch((err) => console.log(err));
+  }
+
   return (
     <>
       <section className="hero">
@@ -21,12 +34,17 @@ export default function Events() {
       <section className="hero">
         <div className="hero-body has-bg-image is-medium">
           <div className="container has-text-centered is-3 is-fullhd is-4-desktop is-12-tablet is-12-mobile has-text-black">
-            <h1 className="title" id="hero-font">
-              {/* {event.event_name} */}
-            </h1>
-            <h2 className="subtitle" id="hero-font">
-              {/* {event.event_date} */}
-            </h2>
+            <h1 className="title" id="hero-font"></h1>
+              {events.map((event) => (
+                <div className="wrapper">
+                <h1 className="columns is-centered has-text-weight-bold" id="event-font">
+                  {event.event_name}
+                </h1>
+                <h1 className="columns is-centered has-text-weight-bold" id="event-font">
+                  {event.event_date}
+                </h1>  
+                </div>              
+                ))}
           </div>
         </div>
       </section>
