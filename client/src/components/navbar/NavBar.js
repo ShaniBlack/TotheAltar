6bd1c325d1af3b5c634fe69976f5ef1aafa2ed54
Shani Black <shaniblack2004@gmail.com>
2021-01-26 20:53:27 -0600

    added toggleBurger function for hamburger menu

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "react-bulma-components/dist/react-bulma-components.min.css";
-// import "./bulma.js";
 import "./navbar.css";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
+
+function toggleBurger() {
+  const burger = document.querySelector(".navbar-burger");
+  const menu = document.querySelector(".navbar-menu");
+  burger.addEventListener("click", function () {
+      burger.classList.toggle("is-active")
+      menu.classList.toggle("is-active")
+});
+};
 
 const NavBar = () => {
   const { user, logout } = useAuth0();
@@ -19,13 +27,14 @@ const NavBar = () => {
             className="button navbar-burger burger"
             data-target="navbar-menu"
             aria-expanded="false"
+            onClick={toggleBurger}
           >
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </div>
         </div>
-        <div className="navbar-menu">
+        <div className="navbar-menu" id="navbar-menu">
           <div className="navbar-end">
             {/* <div className="navbar-brand"> */}
               <a
--- client/src/components/navbar/bulma.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector(".navbar-burger")
-const menu = document.querySelector(".navbar-menu")
-burger.addEventListener("click", function () {
-    burger.classList.toggle("is-active")
-    menu.classList.toggle("is-active")
-})
